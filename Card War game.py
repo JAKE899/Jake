69de93830ac065483015b6af0aec0228d07e3f96
JAKE899 <j.gillett2789@hotmail.com>
2017-01-26 11:35:44 +0000

    Add files via upload

---
+++ Card War game.py
@@ -0,0 +1,33 @@
+deck3 = 3
+deck6 = 6
+deck10 = 10
+apple = 6
+end = "no"
+Next = "yes"
+human_input = input("choose a deck selection from a deck of 3 or 6 or 10 ")
+if human_input == 3:
+    print str(deck3) + "-this is your deck size"
+
+end_choose = input("Do you want to carry on?")
+if end_choose == str(end):
+    print "ok"
+
+
+#while apple<5:
+ #   for i in range(0,6):
+  #      apple = apple + 2
+   #     print apple
+
+#else:
+ #   for i in range(6,11):
+  #      apple = apple + 3 
+   #    print apple
+
+
+    
+    
+     
+
+
+
+#print(str(deck3))
