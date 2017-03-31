c17bc07e37b6b9e69371a1e520a5212066cc52a5
JAKE899 <j.gillett2789@hotmail.com>
2017-03-31 12:24:02 +0100

    Add files via upload

---
+++ homework.py
@@ -0,0 +1,14 @@
+MonthName = ["January","February","March","April","May","June","July",
+             "August","September","October","November","December"]
+
+bill = 23.00
+monthlyMax = 500
+totalminutes = (float(input("What is your total minutes used?")
+extraminutes = (totalminutes - monthlyMax)
+minuteCharge = 0.75
+totalextraminute = 0.75*extraminutes
+totatbill = bill + totalextraminute
+print(totalbill)
+
+
+
